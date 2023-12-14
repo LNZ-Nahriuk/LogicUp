8691a1976e40195cf3cfa2452597aef74f124283
Nazariy-V <153752324+Nazariy-V@users.noreply.github.com>
2023-12-14 22:38:22 +0000

    added app.register_blueprint(main)

--- pymain/__innit__.py
+++ pymain/__innit__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from extensions import mongo
+from main import main
 def create_app(config_object='pymain.settings'):
   app = Flask(__name__)
   app.config.from_object(config_object)
+  app.register_blueprint(main)
   mongo.init_app(app)
   return app
 
